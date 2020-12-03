550af8e9dbd81eda4a5b7632b1e25f4fb99a0dc2
as43z <albertsigmaeng@gmail.com>
2020-12-03 18:14:58 +0100

    main server architecture

--- src/main/java/edu/upc/eetac/dsa/Main.java
+++ src/main/java/edu/upc/eetac/dsa/Main.java
@@ -1,9 +1,101 @@
 package edu.upc.eetac.dsa;
 
-public class Main {
+/*
+*   +--------------+
+*   |   IMPORTS    |
+*   +--------------+
+ */
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
-    public static void main(String[] args) {
-        //main method
+import io.swagger.jaxrs.config.BeanConfig;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    //                      vars
+    //------------------------------------------------------
+    // Logger
+    // vvvvvv
+    static final Logger log = Logger.getLogger(Main.class);
+
+    // Base para los directorios
+    // vvvvvvvvvvvvvvvvvvvvvvvvv
+    static final String PACKAGE_PATH = "edu.upc.eetac.dsa.";
+
+    // Base de la API
+    // vvvvvvvvvvvvvv
+    static final String BASE_PATH = "dsaAPP/";
+
+    // El server HTTP escuchará las peticiones que le lleguen aquí
+    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    public static final String BASE_URI = "http://localhost:8080/" + BASE_PATH;
+
+    // Swagger
+    // vvvvvvv
+    public static final String SWAGGER_URI = BASE_URI.replace(BASE_PATH, "swagger");
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in edu.upc.dsa package
+        final ResourceConfig rc = new ResourceConfig().packages(PACKAGE_PATH + ".services");
+
+        rc.register(io.swagger.jaxrs.listing.ApiListingResource.class);
+        rc.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        BeanConfig beanConfig = new BeanConfig();
+
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath(BASE_PATH);
+        beanConfig.setContact("albert.saez.nunez@estudiantat.upc.edu y et. al.");
+        beanConfig.setDescription("API DSA");
+        beanConfig.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+        beanConfig.setResourcePackage(PACKAGE_PATH + ".services");
+        beanConfig.setTermsOfServiceUrl("http://www.example.com/resources/eula");
+        beanConfig.setTitle("REST API");
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setScan(true);
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     * @param args - Argument
+     * @throws IOException -Throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // configurar el logger
+        PropertyConfigurator.configure("src/main/resources/log4j.properties");
+        log.info("init sys");
+
+        // arrancar el server
+        final HttpServer server = startServer();
+
+        // base
+        StaticHttpHandler staticHttpHandler = new StaticHttpHandler("./public/");
+        server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/");
+
+        // info
+        log.info("API started @ " + BASE_URI);
+        log.info("SWAGGER service @ " + SWAGGER_URI);
+
+        // apagar el server
+        System.out.println("ENTER to stop...");
+        System.in.read();
+        server.shutdownNow();
     }
 
 }
