3e2bb9714a8f813b97bbd1087e297e0f865bfb32
Albert S <albertsigmaeng@gmail.com>
2020-12-04 16:00:13 +0100

    added IP automation support

--- src/main/java/edu/upc/eetac/dsa/Main.java
+++ src/main/java/edu/upc/eetac/dsa/Main.java
@@ -14,7 +14,10 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import java.net.InetAddress;
+
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URI;
 
 public class Main {
@@ -29,17 +32,11 @@ public class Main {
     // Base de la API
     static final String BASE_PATH = "dsaApp/";
 
-    // El server HTTP escuchará las peticiones que le lleguen aquí
-    public static final String BASE_URI = "http://147.83.7.208:8080/" + BASE_PATH;
-
-    // Swagger
-    public static final String SWAGGER_URI = BASE_URI.replace(BASE_PATH, "swagger/");
-
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
-    public static HttpServer startServer() {
+    public static HttpServer startServer(String ipAddr, String BASE_URI) {
         // create a resource config that scans for JAX-RS resources and providers
         // in edu.upc.dsa package
         final ResourceConfig rc = new ResourceConfig().packages(PACKAGE_PATH + "service");
@@ -49,7 +46,7 @@ public class Main {
 
         BeanConfig beanConfig = new BeanConfig();
 
-        beanConfig.setHost("147.83.7.208:8080");
+        beanConfig.setHost(ipAddr + ":8080");
         beanConfig.setBasePath(BASE_PATH);
         beanConfig.setContact("albert.saez.nunez@estudiantat.upc.edu / victor.moreno.barrera@estudiantat.upc.edu / renuka.jain@estudiantat.upc.edu / marc.xapelli@estudiantat.upc.edu / javier.salmeron@estudiantat.upc.edu / guillem.montejo@estudiantat.upc.edu .");
         beanConfig.setDescription("API for game project");
@@ -71,19 +68,31 @@ public class Main {
      * @throws IOException -Throws IOException
      */
     public static void main(String[] args) throws IOException {
+        // config IP, si la IP contiene 147.83.X.X, cambiala, si no, localhost
+        InetAddress machineIP = InetAddress.getLocalHost();
+        String ipAddr = "localhost";
+        if(machineIP.getHostAddress().contains("147.83.")) ipAddr = machineIP.getHostAddress();
+
+        // El server estará aquí:
+        final String BASE_URI = "http://"+ ipAddr +":8080/" + BASE_PATH;
+
+        // Swagger
+        final String SWAGGER_URI = BASE_URI.replace(BASE_PATH, "swagger/");
+
         // configurar el logger
         PropertyConfigurator.configure("src/main/resources/log4j.properties");
+        log.info("Server will be running at interface @IP: " + ipAddr);
         log.info("init sys");
 
         // arrancar el server
-        final HttpServer server = startServer();
+        final HttpServer server = startServer(ipAddr, BASE_URI);
 
         // base
         StaticHttpHandler staticHttpHandler = new StaticHttpHandler("./public/");
         server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/");
 
         // info
-        log.info("Greeting page @ http://147.83.7.208:8080/");
+        log.info("Greeting page @ http://" + ipAddr + ":8080/");
         log.info("API started @ " + BASE_URI);
         log.info("SWAGGER service @ " + SWAGGER_URI);
 
