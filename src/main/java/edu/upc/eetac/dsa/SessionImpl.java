39dbad8c17ade54b2ee2bbb8309605525a603e53
as43z <albertsigmaeng@gmail.com>
2020-12-20 12:21:15 +0100

    session, ohelper, qhelper

--- src/main/java/edu/upc/eetac/dsa/SessionImpl.java
+++ src/main/java/edu/upc/eetac/dsa/SessionImpl.java
@@ -1,7 +1,9 @@
 package edu.upc.eetac.dsa;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import edu.upc.eetac.dsa.utils.ObjectHelper;
+import edu.upc.eetac.dsa.utils.QueryHelper;
+
+import java.sql.*;
 import java.util.List;
 
 public class SessionImpl implements Session {
@@ -15,6 +17,21 @@ public class SessionImpl implements Session {
     @Override
     public void save(Object entity) {
         //SAVE AN OBJECT TO THE DATABASE (Crud)
+        String iQuery = QueryHelper.queryInsert(entity); //INSERT QUERY
+        PreparedStatement pstm = null; //NULL
+
+        try {
+            pstm = conn.prepareStatement(iQuery); //EMPEZAMOS A PREPARARLO
+            int i = 1;
+            for(String field : ObjectHelper.getFields(entity)){
+                Object o = ObjectHelper.getter(entity, field); //Cada Objecto -> devuelve el valor del campo
+                pstm.setObject(i, o); //BUILD
+                i++;
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -30,7 +47,26 @@ public class SessionImpl implements Session {
     @Override
     public Object get(Class c, String ID) {
         //READ AN OBJECT FROM THE DATABASE (cRud)
-        return null;
+        Object o = null;
+        PreparedStatement pstm = null;
+        try{
+            String sQuery =  QueryHelper.querySelect(c);
+
+            o = c.newInstance();
+            pstm = conn.prepareStatement(sQuery);
+            pstm.setObject(1, ID);
+            ResultSet rs = pstm.executeQuery();
+            while(rs.next()){
+                ResultSetMetaData rsMeta = rs.getMetaData();
+                for(int i = 1; i < rsMeta.getColumnCount(); i++){
+                    String prop = rsMeta.getColumnClassName(i);
+                    ObjectHelper.setter(o, prop, rs.getObject(i));
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return o;
     }
 
     /*
--- src/main/java/edu/upc/eetac/dsa/utils/ObjectHelper.java
+++ src/main/java/edu/upc/eetac/dsa/utils/ObjectHelper.java
@@ -1,6 +1,8 @@
 package edu.upc.eetac.dsa.utils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ObjectHelper {
     public static String[] getFields(Object entity) {
@@ -16,5 +18,57 @@ public class ObjectHelper {
         }
 
         return sFields;
+    }
+
+    public static Object setter(Object o, String prop, Object value) throws NoSuchMethodException {
+        Class<?> c = o.getClass();
+
+        try {
+            //BUILD THE METHOD
+            char[] propChar = prop.toCharArray();
+            propChar[0] = Character.toUpperCase(prop.charAt(0));
+            prop = String.valueOf(propChar);
+
+            String gMeth = "set" + prop; //Method
+
+            //USE THE METHOD
+            Method set = null;
+            Class type = value.getClass();
+            set = c.getMethod(gMeth, type);
+            set.invoke(o, value);
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return o;
+    }
+
+    public static Object getter(Object o, String prop) throws NoSuchMethodException {
+        Object val = null;
+        Class c = o.getClass();
+
+        try {
+            //BUILD THE METHOD
+            char[] propChar = prop.toCharArray();
+            propChar[0] = Character.toUpperCase(prop.charAt(0));
+            prop = String.valueOf(propChar);
+
+            String sMeth = "get" + prop; //Method
+
+            //USE THE METHOD
+            Method get = null;
+            get = c.getMethod(sMeth);
+            val = get.invoke(o);
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return val;
     }
 }
