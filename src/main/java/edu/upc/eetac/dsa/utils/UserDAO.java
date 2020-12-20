6fff1e3c46088176d575e662b48d168333410631
VictorMorenoo <victormoreno98.vm@gmail.com>
2020-12-20 10:42:41 +0000

    dao

--- src/main/java/edu/upc/eetac/dsa/utils/UserDAO.java
+++ src/main/java/edu/upc/eetac/dsa/utils/UserDAO.java
@@ -1,6 +1,14 @@
 package edu.upc.eetac.dsa.utils;
+import edu.upc.eetac.dsa.*;
 
-public interface IntUserDAO
+public interface UserDAO
 {
-    public int
+
+    public int addUser(String name, String surname, double salary);
+    public User getUser(int employeeID);
+    public void updateUser(int employeeID, String name, String surname, double salary);
+    public void deleteUser(int employeeID);
+    public List<User> getUsers();
+    public List <User> getEmployeeByDept(int deptId);
+
 }
--- src/main/java/edu/upc/eetac/dsa/utils/UserDAOImpl.java
+++ src/main/java/edu/upc/eetac/dsa/utils/UserDAOImpl.java
@@ -1,7 +1,120 @@
 package edu.upc.eetac.dsa.utils;
 
-public class UserDAO
-{
+import edu.upc.eetac.dsa.models.User;
+import java.util.HashMap;
+import java.util.List;
 
+public class UserDAOImpl implements UserDAO
+{
+    public int addUser(String name, String surname, double salary) {
+        Session session = null;
+        int employeeID = 0;
+        try {
+            session = FactorySession.openSession();
+            User employee = new User(name, surname, salary);
+            session.save(employee);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+
+        return employeeID;
+    }
+
+
+    public User getUser(int employeeID) {
+        Session session = null;
+        Employee employee = null;
+        try {
+            session = FactorySession.openSession();
+            employee = (User)session.get(User.class, employeeID);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+
+        return employee;
+    }
+
+
+    public void updateUser(int employeeID, String name, String surname, double salary) {
+        User u = this.getUser(employeeID);
+        u.setName(name);
+        u.setSurname(surname);
+        u.setSalary(salary);
+
+        Session session = null;
+        try {
+            session = FactorySession.openSession();
+            session.update(User.class);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    public void deleteUser(int employeeID) {
+        User u = this.getEmploye(employeeID);
+        Session session = null;
+        try {
+            session = FactorySession.openSession();
+            session.delete(Employee.class);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+
+    }
+
+
+    public List<Employee> getEmployees() {
+        Session session = null;
+        List<Employee> employeeList=null;
+        try {
+            session = FactorySession.openSession();
+            employeeList = session.findAll(Employee.class);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+        return employeeList;
+    }
+
+
+    public List<Employee> getEmployeeByDept(int deptID) {
+
+        Session session = null;
+        List<Employee> employeeList=null;
+        try {
+            session = FactorySession.openSession();
+
+            HashMap<String, Integer> params = new HashMap<String, Integer>();
+            params.put("deptID", deptID);
+
+            employeeList = session.findAll(Employee.class, params);
+        }
+        catch (Exception e) {
+            // LOG
+        }
+        finally {
+            session.close();
+        }
+        return employeeList;
+    }
 
 }
