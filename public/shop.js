9dd231e7b300b9a52f903ff6d389f26389d7d0da
Guille Montejo <guillemmontejo@gmail.com>
2021-01-25 11:41:59 +0100

    Actualización web

--- public/shop.js
+++ public/shop.js
@@ -1,12 +1,15 @@
+var inventory = [];
+let userId = sessionStorage.getItem('ID');
+
 $(document).ready(function(){
-  let userId = sessionStorage.getItem('ID');
   if(userId == null){
       window.location.replace("/");
     }
   var mokData = [];
   var userData;
 
-  var userURL = '/dsaApp/User/' + userId;
+
+  //Call for user data, to get the user cash.
   $.ajax({
     url: '/dsaApp/User/' + userId,
     dataType: 'json',
@@ -19,7 +22,21 @@ $(document).ready(function(){
     error: function(xhr,resp,text){
         alert("Error getting the user cash!");
     }
-  })
+  });
+
+  //Call for user inventory
+  $.ajax({
+      url: '/dsaApp/User/' + userId + '/Inventory',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(x) {
+          console.log(x);
+          inventory = [x.bookQuantity,x.calculatorQuantity,x.cheatQuantity,x.coffQuantity,x.compassQuantity,x.glassesQuantity,x.pencilQuantity,x.pillsQuantity,x.puzzleQuantity,x.redbullQuantity,x.ruleQuantity,x.turtleQuantity,x.usbQuantity];
+          },
+      error: function(xhr,resp,text){
+          alert("Error while getting the inventory info!");
+      }
+    });
 
 
   $.ajax({
@@ -39,8 +56,53 @@ $(document).ready(function(){
       }
   });
 
+  $('TurtleClick').click(function(){
+    var turtleVal = inventory[11];
+    turtleVal = turtleVal + 1;
+    inventory[11] = turtleVal;
+
+  });
+
+  $('CoffeeClick').click(function(){
+  });
+
+  $('RedBullClick').click(function(){
+  });
+
+  $('PillsClick').click(function(){
+  });
+
+  $('CalculatorClick').click(function(){
+  });
+
+
   $('#logout').click(function() {
     sessionStorage.removeItem("Nombre");
     sessionStorage.removeItem("ID");
   });
+
+
 });
+
+function updateInventory(inventory){
+
+    var obj = {turtleQuantity: inventory[], coffQuantity: inventory[], redbullQuantity: inventory[], pillsQuantity: inventory[], calculatorQuantity: inventory[], ruleQuantity: inventory[], compassQuantity: inventory[], pencilQuantity: inventory[], glassesQuantity: inventory[], puzzleQuantity: inventory[], bookQuantity: inventory[], usbQuantity: inventory[], cheatQuantity: inventory[], id: ""}
+    var myJSON = JSON.stringify(obj);
+
+    $.ajax({
+      type: 'PATCH',
+      headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE"},
+      url: '/dsaApp/User/'+userId+'/UpdateInventory',
+      dataType: 'json',
+      data: myJSON,
+      contentType: 'application/json',
+      success: function(x) {
+        console.log(x);
+        alert("success");
+      },
+      error: function(xhr,resp,text){
+        console.log(xhr,resp,text);
+        alert("There is a problem.");
+      }
+    });
+}
